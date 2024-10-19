e40216c5bef87e7b19293b1be3f9cb5393c2f794
sabrina kassie <sabrinakassie@gmail.com>
2024-10-18 22:13:57 -0400

    commit5

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -4,7 +4,11 @@ from .user import user_views
 from .index import index_views
 from .auth import auth_views
 from .admin import setup_admin
+from .recruiter import recruiter_views
+from .job import job_views
+from .application import application_views
+from .applicant import applicant_views
 
 
-views = [user_views, index_views, auth_views] 
+views = [user_views, index_views, auth_views,recruiter_views,job_views,application_views,applicant_views] 
 # blueprints must be added to this list
